718d723f4cc14f3589c6326a9e54514950a8121a
Рождественский Андрей <andrey.rozhdestvensky@corp.ktelabs.ru>
2025-01-16 22:07:52 +0300

    init

---
+++ src/test/java/com/rozhdev/ordee/OrdeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozhdev.ordee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
