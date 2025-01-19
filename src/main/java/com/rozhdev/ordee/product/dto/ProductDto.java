7ee46f244dd2706907517271cb25f088f17e281a
Рождественский Андрей <andrey.rozhdestvensky@corp.ktelabs.ru>
2025-01-19 14:49:27 +0300

    CORE-1 change validation price on positive

--- src/main/java/com/rozhdev/ordee/product/dto/ProductDto.java
+++ src/main/java/com/rozhdev/ordee/product/dto/ProductDto.java
@@ -1,10 +1,7 @@
 package com.rozhdev.ordee.product.dto;
 
 import com.rozhdev.ordee.commons.validation.Create;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +10,6 @@ public record ProductDto(
         Long id,
         @NotBlank
         String name,
-        @NotNull @Min(value = 0, message = "Price must be positive")
+        @Positive(message = "Price must be positive")
         BigDecimal price
 ) {}
