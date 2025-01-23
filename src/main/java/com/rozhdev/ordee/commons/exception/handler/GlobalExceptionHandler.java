55f6bdf40bf14904277d8ecaf0b1ff38874cafcf
Рождественский Андрей <andrey.rozhdestvensky@corp.ktelabs.ru>
2025-01-23 23:08:55 +0300

    ORDEE-1 handle exception

---
+++ src/main/java/com/rozhdev/ordee/commons/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.rozhdev.ordee.commons.exception.handler;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.rozhdev.ordee.commons.exception.ErrorDetail;
+import com.rozhdev.ordee.commons.exception.ErrorDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+import static com.rozhdev.ordee.commons.exception.Status.ERROR;
+import static com.rozhdev.ordee.commons.exception.Status.INVALID;
+import static org.springframework.http.HttpStatus.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<ErrorDetail> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> new ErrorDetail(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+        ErrorDto errorDto = new ErrorDto(INVALID, UNPROCESSABLE_ENTITY.value(), "Invalid input data", errors);
+        return ResponseEntity
+                .status(UNPROCESSABLE_ENTITY)
+                .body(errorDto);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        List<ErrorDetail> errors = List.of();
+        if (ex.getCause() instanceof JsonMappingException jsonMappingException) {
+            errors = jsonMappingException.getPath().stream()
+                    .map(reference -> new ErrorDetail(reference.getFieldName(), "Unknown field"))
+                    .toList();
+        }
+        ErrorDto errorDto = new ErrorDto(ERROR, BAD_REQUEST.value(), "Invalid json format", errors);
+        return ResponseEntity
+                .badRequest()
+                .body(errorDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleGeneralException(Exception ex) {
+        ErrorDto errorDto = new ErrorDto(ERROR, INTERNAL_SERVER_ERROR.value(), "Internal Server Error", null);
+        return ResponseEntity
+                .status(INTERNAL_SERVER_ERROR)
+                .body(errorDto);
+    }
+}
